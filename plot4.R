31f72d45cd82f4183d096b785cb5d07b47dd67f3
Maria Demesa <maria.demesa@att.net>
2014-06-06 11:31:17 -0400

    Add plot4.R

---
+++ plot4.R
@@ -0,0 +1,47 @@
+# Filter the columnames
+colnam <- read.csv("household_power_consumption.txt",nrows=1,sep=";")
+# Read a smaller amount of data that includes the data to be analyze
+data<-read.csv("household_power_consumption.txt",skip=66312 ,nrows=4000,sep=";",header=FALSE,na.strings="?")
+colnames(data)<-colnames(colnam)
+data[,2]<-paste(data[,1],data[,2])
+data[,1]<-as.Date(data[,1],format="%d/%m/%Y")
+# Filter the data that is going to be included in the plot
+fildat<-data[which(data$Date %in% as.Date(c('2007-02-01','2007-02-02'))),]
+# Open the device
+png(filename="plot4.png")
+# Set the panels to four
+par(mfrow = c(2,2))
+par(mar=c(4,4,2,2))
+# Finally plot the data
+plot(strptime(fildat[,2],format="%d/%m/%Y %H:%M:%S"),
+     fildat[,3],
+     type="l",
+     xlab="",
+     ylab="Global Active Power")
+
+plot(strptime(fildat[,2],format="%d/%m/%Y %H:%M:%S"),
+     fildat[,5],
+     type="l",
+     xlab="datetime",
+     ylab="Voltage")
+
+
+plot(strptime(fildat[,2],format="%d/%m/%Y %H:%M:%S"),
+     fildat[,7],
+     type="l",
+     xlab="",
+     ylab="Energy sub metering")
+lines(strptime(fildat[,2],format="%d/%m/%Y %H:%M:%S"),
+      fildat[,8],type="l",col="red")
+lines(strptime(fildat[,2],format="%d/%m/%Y %H:%M:%S"),
+      fildat[,9],type="l",col="blue")
+
+legend("topright", colnames(colnam[7:9]), col=c("black","red","blue"),lwd=2,bty="n") 
+
+plot(strptime(fildat[,2],format="%d/%m/%Y %H:%M:%S"),
+     fildat[,4],
+     type="l",
+     xlab="datetime",
+     ylab="Global_reactive_power")
+# close the device
+dev.off()
